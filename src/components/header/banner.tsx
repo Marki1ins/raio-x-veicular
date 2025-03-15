41c7002976c5236f3ef97a0f67d8b9da82963d71
Marki1ins <marcosdfcarvalhoo@gmail.com>
2025-03-14 22:22:02 -0300

    refactor: :lipstick: change color layer

--- src/components/header/banner.tsx
+++ src/components/header/banner.tsx
@@ -1,25 +1,14 @@
 import Container from '@/layout';
-import { FaUser } from 'react-icons/fa6';
+
+import BannerMarquee from '../banner-marquee';
 
 export default function Banner() {
   return (
     <>
-      <div className="flex items-center justify-center overflow-hidden">
-        <div className="flex items-center whitespace-nowrap animate-marquee py-3 bg-gray-200">
-          {Array.from({ length: 15 }).map((_, index) => (
-            <div
-              key={index}
-              className="flex items-center pr-8 space-x-4 text-blue-800 text-xs"
-            >
-              <FaUser />
-              <p>Fulano de tal - Brasília</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      <BannerMarquee data={Array.from({ length: 15 })} />
       <div className='bg-blue-50'>
         <Container>
-            <div className='flex items-center justify-between py-4 text-gray-600 text-[10px] font-bold md:text-xs'>
+            <div className='flex items-center justify-between py-4 text-gray-700 text-[10px] font-bold md:text-xs'>
                 <div>58 pessoas obtendo serviços MEI Agora</div>
                 <div>118.330 MEI Emitidos</div>
             </div>
