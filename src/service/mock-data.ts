918c220ff29ba4a05b77bf903ec58c988fe6922f
Marki1ins <marcosdfcarvalhoo@gmail.com>
2025-03-14 21:14:13 -0300

    refactor: :recycle: add mock data file

---
+++ src/service/mock-data.ts
@@ -0,0 +1,152 @@
+export const tableData = [
+  {
+    id: 1,
+    name: "Dados cadastrais estaduais",
+    completed: true,
+    essential: true,
+    vehicle_data: true,
+  },
+  {
+    id: 2,
+    name: "Dados cadastrais nacionais",
+    completed: true,
+    essential: true,
+    vehicle_data: true,
+  },
+  {
+    id: 3,
+    name: "Aceitação em seguro",
+    completed: true,
+    essential: true,
+    vehicle_data: true,
+  },
+  {
+    id: 4,
+    name: "Dados de leilão",
+    completed: true,
+    essential: true,
+    vehicle_data: true,
+  },
+  {
+    id: 5,
+    name: "Score de leilão",
+    completed: true,
+    essential: true,
+    vehicle_data: true,
+  },
+  {
+    id: 6,
+    name: "Ficha técnica",
+    completed: true,
+    essential: true,
+    vehicle_data: true,
+  },
+  {
+    id: 7,
+    name: "Risco de comercialização",
+    completed: true,
+    essential: false,
+    vehicle_data: false,
+  },
+  {
+    id: 8,
+    name: "Batidas",
+    completed: true,
+    essential: false,
+    vehicle_data: false,
+  },
+  {
+    id: 9,
+    name: "Roubo e furto",
+    completed: true,
+    essential: false,
+    vehicle_data: false,
+  },
+  {
+    id: 10,
+    name: "Débitos e multas",
+    completed: true,
+    essential: false,
+    vehicle_data: false,
+  },
+  {
+    id: 11,
+    name: "Restrições e impedimentos",
+    completed: true,
+    essential: false,
+    vehicle_data: false,
+  },
+  {
+    id: 12,
+    name: "Risco de comercialização",
+    completed: true,
+    essential: false,
+    vehicle_data: false,
+  },
+  {
+    id: 13,
+    name: "Gravame",
+    completed: true,
+    essential: false,
+    vehicle_data: false,
+  },
+  {
+    id: 14,
+    name: "Recall",
+    completed: true,
+    essential: false,
+    vehicle_data: false,
+  },
+  {
+    id: 15,
+    name: "Renajud detalhe",
+    completed: true,
+    essential: false,
+    vehicle_data: false,
+  },
+];
+
+export const accordionData = [
+  {
+    id: 1,
+    title: "Como fazer a consulta?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque porttitor aliquam. Mauris convallis viverra arcu eu sollicitudin. Nullam congue posuere arcu. Donec dolor justo, interdum vitae suscipit quis, aliquet vitae nunc. Aenean et pulvinar lectus. Etiam egestas sit amet ligula in tempus. Duis ac tellus a nulla aliquet pellentesque eu eget augue. Nam eros dolor, pharetra finibus dapibus at, pharetra vehicula massa. Nunc blandit tristique finibus. Nulla a quam quam. Vestibulum ac ultrices nisi, a tincidunt quam. Nam tempus mi sit amet sem porttitor, sit amet sodales magna aliquet. Fusce eu lacus eu tellus scelerisque placerat. Donec consequat, ligula ac dapibus dapibus, est eros pretium neque, vitae porttitor tortor eros ac odio. Nunc tempor ipsum massa, et vestibulum diam ornare at. Morbi iaculis consectetur blandit.",
+  },
+  {
+    id: 2,
+    title: "Quantas consultas posso fazer?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque porttitor aliquam. Mauris convallis viverra arcu eu sollicitudin. Nullam congue posuere arcu. Donec dolor justo, interdum vitae suscipit quis, aliquet vitae nunc. Aenean et pulvinar lectus. Etiam egestas sit amet ligula in tempus. Duis ac tellus a nulla aliquet pellentesque eu eget augue. Nam eros dolor, pharetra finibus dapibus at, pharetra vehicula massa. Nunc blandit tristique finibus. Nulla a quam quam. Vestibulum ac ultrices nisi, a tincidunt quam. Nam tempus mi sit amet sem porttitor, sit amet sodales magna aliquet. Fusce eu lacus eu tellus scelerisque placerat. Donec consequat, ligula ac dapibus dapibus, est eros pretium neque, vitae porttitor tortor eros ac odio. Nunc tempor ipsum massa, et vestibulum diam ornare at. Morbi iaculis consectetur blandit.",
+  },
+  {
+    id: 3,
+    title: "É seguro consultar no Raio X Veicular?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque porttitor aliquam. Mauris convallis viverra arcu eu sollicitudin. Nullam congue posuere arcu. Donec dolor justo, interdum vitae suscipit quis, aliquet vitae nunc. Aenean et pulvinar lectus. Etiam egestas sit amet ligula in tempus. Duis ac tellus a nulla aliquet pellentesque eu eget augue. Nam eros dolor, pharetra finibus dapibus at, pharetra vehicula massa. Nunc blandit tristique finibus. Nulla a quam quam. Vestibulum ac ultrices nisi, a tincidunt quam. Nam tempus mi sit amet sem porttitor, sit amet sodales magna aliquet. Fusce eu lacus eu tellus scelerisque placerat. Donec consequat, ligula ac dapibus dapibus, est eros pretium neque, vitae porttitor tortor eros ac odio. Nunc tempor ipsum massa, et vestibulum diam ornare at. Morbi iaculis consectetur blandit.",
+  },
+  {
+    id: 4,
+    title: "Quais tipos de veículos podem ser consultados?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque porttitor aliquam. Mauris convallis viverra arcu eu sollicitudin. Nullam congue posuere arcu. Donec dolor justo, interdum vitae suscipit quis, aliquet vitae nunc. Aenean et pulvinar lectus. Etiam egestas sit amet ligula in tempus. Duis ac tellus a nulla aliquet pellentesque eu eget augue. Nam eros dolor, pharetra finibus dapibus at, pharetra vehicula massa. Nunc blandit tristique finibus. Nulla a quam quam. Vestibulum ac ultrices nisi, a tincidunt quam. Nam tempus mi sit amet sem porttitor, sit amet sodales magna aliquet. Fusce eu lacus eu tellus scelerisque placerat. Donec consequat, ligula ac dapibus dapibus, est eros pretium neque, vitae porttitor tortor eros ac odio. Nunc tempor ipsum massa, et vestibulum diam ornare at. Morbi iaculis consectetur blandit.",
+  },
+  {
+    id: 5,
+    title: "As informações fornecidas são confiáveis?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque porttitor aliquam. Mauris convallis viverra arcu eu sollicitudin. Nullam congue posuere arcu. Donec dolor justo, interdum vitae suscipit quis, aliquet vitae nunc. Aenean et pulvinar lectus. Etiam egestas sit amet ligula in tempus. Duis ac tellus a nulla aliquet pellentesque eu eget augue. Nam eros dolor, pharetra finibus dapibus at, pharetra vehicula massa. Nunc blandit tristique finibus. Nulla a quam quam. Vestibulum ac ultrices nisi, a tincidunt quam. Nam tempus mi sit amet sem porttitor, sit amet sodales magna aliquet. Fusce eu lacus eu tellus scelerisque placerat. Donec consequat, ligula ac dapibus dapibus, est eros pretium neque, vitae porttitor tortor eros ac odio. Nunc tempor ipsum massa, et vestibulum diam ornare at. Morbi iaculis consectetur blandit.",
+  },
+  {
+    id: 6,
+    title: "As informações fornecidas são confiáveis?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque porttitor aliquam. Mauris convallis viverra arcu eu sollicitudin. Nullam congue posuere arcu. Donec dolor justo, interdum vitae suscipit quis, aliquet vitae nunc. Aenean et pulvinar lectus. Etiam egestas sit amet ligula in tempus. Duis ac tellus a nulla aliquet pellentesque eu eget augue. Nam eros dolor, pharetra finibus dapibus at, pharetra vehicula massa. Nunc blandit tristique finibus. Nulla a quam quam. Vestibulum ac ultrices nisi, a tincidunt quam. Nam tempus mi sit amet sem porttitor, sit amet sodales magna aliquet. Fusce eu lacus eu tellus scelerisque placerat. Donec consequat, ligula ac dapibus dapibus, est eros pretium neque, vitae porttitor tortor eros ac odio. Nunc tempor ipsum massa, et vestibulum diam ornare at. Morbi iaculis consectetur blandit.",
+  },
+  {
+    id: 7,
+    title: "O que recebo ao pagar por uma consuta?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque porttitor aliquam. Mauris convallis viverra arcu eu sollicitudin. Nullam congue posuere arcu. Donec dolor justo, interdum vitae suscipit quis, aliquet vitae nunc. Aenean et pulvinar lectus. Etiam egestas sit amet ligula in tempus. Duis ac tellus a nulla aliquet pellentesque eu eget augue. Nam eros dolor, pharetra finibus dapibus at, pharetra vehicula massa. Nunc blandit tristique finibus. Nulla a quam quam. Vestibulum ac ultrices nisi, a tincidunt quam. Nam tempus mi sit amet sem porttitor, sit amet sodales magna aliquet. Fusce eu lacus eu tellus scelerisque placerat. Donec consequat, ligula ac dapibus dapibus, est eros pretium neque, vitae porttitor tortor eros ac odio. Nunc tempor ipsum massa, et vestibulum diam ornare at. Morbi iaculis consectetur blandit.",
+  },
+];
